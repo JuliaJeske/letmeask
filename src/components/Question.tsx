b2dc6277a3b8bb31dcc4aa766837000f48a5e42b
juliajeske <julia.jeske@wehandle.com.br>
2022-05-12 10:41:11 -0300

    finalizacion of the app and hosting creation

--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -1,28 +1,43 @@
+import { ReactNode } from 'react';
+import cx from 'classnames';
 import '../styles/questions.scss'
-import { ReactNode} from 'react'
 
-
-type QuestionProps ={
-  content:string;
+type QuestionProps = {
+  content: string;
   author: {
-    name:string;
+    name: string;
     avatar: string;
   };
   children?: ReactNode;
+  isAnswered?: boolean;
+  isHighlighted?: boolean;
 }
 
-
-export function Question(props: QuestionProps) {
-  return(
-    <div className="question">
-      <p>{props.content}</p>
+export function Question({
+  content,
+  author,
+  isAnswered = false,
+  isHighlighted = false,
+  children,
+}: QuestionProps) {
+  return (
+    <div 
+      className={cx(
+        'question', 
+        { answered: isAnswered },
+        { highlighted: isHighlighted && !isAnswered },
+      )}
+    >
+      <p>{content}</p>
       <footer>
         <div className="user-info">
-          <img src={props.author.avatar} alt={props.author.name} />
-          <span>{props.author.name}</span>
+          <img src={author.avatar} alt={author.name} />
+          <span>{author.name}</span>
         </div>
-        <div>{props.children}</div>
+        <div>
+          {children}
+        </div>
       </footer>
     </div>
-  )
+  );
 }
